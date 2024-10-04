66af8f99fc20f48d4dc5c386a629963faf966a9e
McCambley <jakemccamb16@gmail.com>
2024-10-04 08:32:51 -0400

    Refactor form styling and components
    - Removed commented out code and unused imports
    - Updated form styling using Emotion CSS-in-JS
    - Replaced div elements with custom styled FormGroup component
    - Replaced input elements with custom styled Input component
    - Replaced button elements with custom styled Button component

--- src/components/lib.js
+++ src/components/lib.js
@@ -1,6 +1,36 @@
-export * from './lib.final'
+// export * from './lib.final'
+import {jsx} from '@emotion/core'
+import styled from '@emotion/styled'
 
-// export * from './lib.exercise'
+export * from './lib.exercise'
+export const FormGroup = styled.div(props => {
+  return {
+    // outline: '1px solid red',
+    display: 'flex',
+    flexDirection: 'column',
+  }
+})
+
+export const Input = styled.input(props => {
+  return {
+    // outline: '1px solid red',
+    backgroundColor: 'rgb(241, 242, 247)',
+    borderRadius: '3px',
+    padding: '8px 12px',
+    border: 'none',
+  }
+})
+
+export const Button = styled.button(props => ({
+  backgroundColor: props.variant === 'primary' ? 'rgb(63, 81, 181)' : '',
+  color: props.variant === 'primary' ? '#FFFFFF' : '',
+  border: 'none',
+  padding: '10px 15px',
+  height: '36px',
+  lineHeight: '1',
+  width: '90px',
+  borderRadius: '3px',
+}))
 
 // 💯 use the emotion macro
 // export * from './lib.extra-1'
