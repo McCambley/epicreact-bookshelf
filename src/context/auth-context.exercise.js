2a45cbb90cc6bf4f68e4480fff75cd4e2f1cf4a3
McCambley <jakemccamb16@gmail.com>
2024-11-06 19:35:10 -0500

    Complete EC2

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,13 +1,83 @@
 import * as React from 'react'
+import * as auth from '../auth-provider'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
+import {queryCache} from 'react-query'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from '../styles/colors'
 
 const AuthContext = React.createContext()
 
 function useAuth() {
   const context = React.useContext(AuthContext)
   if (context === undefined) {
-    throw Error('useAuth must be used in a child of AuthContext.Provider')
+    throw new Error(`useAuth must be used within a AuthContext provider`)
   }
   return context
 }
 
-export {AuthContext, useAuth}
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider({children}) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    queryCache.clear()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    const props = {user, login, register, logout}
+    return <AuthContext.Provider value={props}>{children}</AuthContext.Provider>
+  }
+}
+
+export {AuthContext, useAuth, AuthProvider}
