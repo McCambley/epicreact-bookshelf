e6fdefd3cf88d858fb68a38f981ba71c99674e6c
McCambley <jakemccamb16@gmail.com>
2024-11-11 00:05:05 -0500

    add unit test for formatDate function

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  const today = new Date('November 11, 2024')
+  expect(typeof formatDate(today)).toBe('string')
+  expect(formatDate(today)).toBe('Nov 24')
+})
