7ff2eaa2059d114f4960c42aefa31385540a84bf
McCambley <jakemccamb16@gmail.com>
2024-11-16 23:01:20 -0500

    We out here testing hooks

--- src/utils/__tests__/use-async.exercise.js
+++ src/utils/__tests__/use-async.exercise.js
@@ -1,18 +1,18 @@
 // 🐨 We'll use renderHook rather than render here
-// import {renderHook, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react'
 // 🐨 Here's the thing you'll be testing:
-// import {useAsync} from '../hooks'
+import {useAsync} from '../hooks'
 
 // 💰 I'm going to give this to you. It's a way for you to create a promise
 // which you can imperatively resolve or reject whenever you want.
-// function deferred() {
-//   let resolve, reject
-//   const promise = new Promise((res, rej) => {
-//     resolve = res
-//     reject = rej
-//   })
-//   return {promise, resolve, reject}
-// }
+function deferred() {
+  let resolve, reject
+  const promise = new Promise((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+  return {promise, resolve, reject}
+}
 
 // Use it like this:
 // const {promise, resolve} = deferred()
@@ -23,7 +23,56 @@
 // do stuff/make assertions you want to after the promise has resolved
 
 // 🐨 flesh out these tests
-test.todo('calling run with a promise which resolves')
+test('calling run with a promise which resolves', async () => {
+  const {promise, resolve} = deferred()
+  const {result} = renderHook(() => useAsync())
+  console.log(result.current)
+  let p
+  act(() => {
+    p = result.current.run(promise)
+  })
+
+  const value = Symbol({val: 'val'})
+  await act(async () => {
+    resolve(value)
+    await p
+  })
+  expect(result.current).toEqual({
+    status: 'resolved',
+    data: value,
+    error: null,
+
+    isIdle: false,
+    isLoading: false,
+    isError: false,
+    isSuccess: true,
+
+    setData: expect.any(Function),
+    setError: expect.any(Function),
+    run: expect.any(Function),
+    reset: expect.any(Function),
+  })
+
+  act(() => {
+    result.current.reset()
+  })
+
+  expect(result.current).toEqual({
+    status: 'idle',
+    data: null,
+    error: null,
+
+    isIdle: true,
+    isLoading: false,
+    isError: false,
+    isSuccess: false,
+
+    setData: expect.any(Function),
+    setError: expect.any(Function),
+    run: expect.any(Function),
+    reset: expect.any(Function),
+  })
+})
 // 🐨 get a promise and resolve function from the deferred utility
 // 🐨 use renderHook with useAsync to get the result
 // 🐨 assert the result.current is the correct default state
