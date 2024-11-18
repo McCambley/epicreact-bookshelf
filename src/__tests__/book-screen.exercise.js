e0d708c0bce048c9ee489057ce50b099c91e3c0d
McCambley <jakemccamb16@gmail.com>
2024-11-17 20:33:14 -0500

    Complete EC2

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {queryCache} from 'react-query'
 import * as auth from 'auth-provider'
 import {buildUser, buildBook} from 'test/generate'
@@ -8,6 +9,7 @@ import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
 import {AppProviders} from 'context'
 import {App} from 'app'
+import {formatDate} from 'utils/misc'
 
 // general cleanup
 afterEach(async () => {
@@ -61,4 +63,46 @@ test('renders all the book information', async () => {
   ).not.toBeInTheDocument()
   expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
   expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
+
+test('Can create a list item for a book', async () => {
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'Test page', route)
+
+  render(<App />, {wrapper: AppProviders})
+
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+
+  const addToListButton = screen.getByRole('button', {name: /add to list/i})
+  await userEvent.click(addToListButton)
+  expect(addToListButton).toBeDisabled()
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+
+  expect(screen.getByLabelText(/mark as read/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/remove from list/i)).toBeInTheDocument()
+  expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
+
+  const startDateNode = screen.getByLabelText(/start date/i)
+  expect(startDateNode).toHaveTextContent(formatDate(new Date()))
+
+  expect(
+    screen.queryByRole('button', {name: /add to list/}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/})).not.toBeInTheDocument()
+  //   screen.debug()
 })
