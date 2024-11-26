c85ab7ff9d1f79e6931fe5933e1176f14e7aab16
McCambley <jakemccamb16@gmail.com>
2024-11-25 21:07:35 -0500

    Remove files from 'exercises/06-cache-management' for final merging of working branches

--- src/components/book-row.js
+++
@@ -1,6 +0,0 @@
-// export * from './book-row.final'
-
-export * from './book-row.exercise'
-
-// 💯 Make hooks
-// export * from './book-row.extra-1'
--- src/index.js
+++
@@ -1,6 +0,0 @@
-// no final
-
-export * from './index.exercise'
-
-// 💯 Wrap the `<App />` in a `<ReactQueryConfigProvider />`
-// export * from './index.extra-2'
