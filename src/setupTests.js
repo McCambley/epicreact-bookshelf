0f1382b658da1b0348ca9e5e5fd28a7add1ef679
McCambley <jakemccamb16@gmail.com>
2024-11-25 21:10:44 -0500

    Remove files from 'exercises/11-unit-testing' for final merging of working branches

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// no final
-
-export * from './setupTests.exercise'
-
-// 💯 Use `setupTests.js`
-// export * from './setupTests.extra-2'
--- src/utils/__tests__/api-client.js
+++
@@ -1,9 +0,0 @@
-// export * from './api-client.final'
-
-export * from './api-client.exercise'
-
-// 💯 Test failure cases
-// export * from './api-client.extra-1'
-
-// 💯 Use `setupTests.js`
-// export * from './api-client.extra-2'
